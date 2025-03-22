4d83ba350bcee71a11c06d4f33d78953c988fcaa
Amen Ouannes <amoua17@ulaval.ca>
2025-03-22 18:53:52 -0400

    backend/login-signup-getusers

--- backend/app/infrastructure/repository.py
+++
@@ -1,9 +0,0 @@
-class Repository:
-    def __init__(self):
-        self.users = []
-        self.songs = []
-        self.playlists = []
-        self.albums = []
-
-    def addUser(self, user):
-        self.users.append(user)
