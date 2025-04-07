6b75c5c0263d4576cd2f69de1d54445255564863
Amen Ouannes <amoua17@ulaval.ca>
2025-04-07 14:49:42 -0400

    database/fix query to get album songs

--- backend/app/domain/Playlist.py
+++ backend/app/domain/Playlist.py
@@ -2,10 +2,21 @@ from collections import OrderedDict
 
 
 class Playlist:
-    def __init__(self, playlist_name, owner, private):
+
+    def __init__(self):
+        self.a=""
+
+    def fromRequest(self, playlist_name, owner):
         self.playlist_name = playlist_name
         self.owner = owner
-        self.private = private
+        return self
+
+    def fromSQL(self, playlistSQL):
+        self.playlist_name = playlistSQL.playlist_name
+        self.owner = playlistSQL.owner
+        self.private = playlistSQL.private
+        self.playlist_id = playlistSQL.playlist_id
+        return self
 
     def to_dict(self):
         return OrderedDict([
---
+++ backend/app/infrastructure/PlaylistSQL.py
@@ -0,0 +1,6 @@
+class PlaylistSQL:
+    def __init__(self, playlist_id,playlist_name, owner, private):
+        self.playlist_id = playlist_id
+        self.playlist_name = playlist_name
+        self.owner = owner
+        self.private = private
